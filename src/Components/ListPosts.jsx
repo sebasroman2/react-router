0d1a750996677330c4def0248202a5fa4b3581f9
Juan Sebastián Román Paz <sebastianromanpaz@gmail.com>
2024-12-15 11:43:21 -0500

    Curso Finalizado

---
+++ src/Components/ListPosts.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+
+import "../assets/css/componentes/card.css"
+import { buscar } from "../api/api"
+
+const ListPosts = ({ url }) => {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        buscar(url, setPosts)
+    }, [url])
+
+    return (
+        <section className="posts container">
+            {
+                posts.map(post => {
+                    const { id, title, metadescription, categoria } = post;
+                    return <Link to={`/posts/${id}`} className={`post__card post-card--${categoria}`} key={id}>
+                        <article>
+                            <h3 className="post-card__title">{title}</h3>
+                            <p className="post-card__meta">{metadescription}</p>
+                        </article>
+                    </Link>
+                })
+            }
+        </section>
+    )
+}
+
+export default ListPosts
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import ListPosts from "../Components/ListPosts"
+import ListCategories from "../Components/ListCategories"
+
+const Home = () => {
+  return (
+    <main>
+      <div className="container">
+        <h2 className="title-page">Pet noticias</h2>
+      </div>
+      <ListCategories />
+      <ListPosts url={"/posts"} />
+    </main>
+  )
+}
+
+export default Home
